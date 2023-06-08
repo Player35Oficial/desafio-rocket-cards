b5c71f877643c5d500902aa6e0c8f4a85e54066b
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-06-08 18:13:43 -0300

    Estrutura do card e implementacao da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import rocketLogo from "./assets/logo.svg";
+import company from "./assets/company.svg";
+import followers from "./assets/followers.svg";
+import following from "./assets/following.svg";
+import location from "./assets/location.svg";
+import repository from "./assets/repository.svg";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const url = "https://api.github.com/users/player35Oficial";
+
+  const fetchUser = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      console.error("Error ao buscar usuário: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, [url]);
+  console.log(user);
   return (
     <div className="main-container">
       <div className="card-container">
@@ -10,9 +34,40 @@ function App() {
             <div className="header-logo">
               <img src={rocketLogo} alt="img-github" />
             </div>
-            <p>UserName</p>
+            <p>{user.login}</p>
           </header>
-          <main>main content</main>
+          <main>
+            <section className="img-container">
+              <img
+                src={user.avatar_url}
+                alt="imagem de perfil do usuário Player35Oficial no Github"
+              />
+            </section>
+            <section className="profile-container">
+              <ul>
+                <li>
+                  <img src={followers} alt="" />
+                  <p>{user.followers} Seguidores</p>
+                </li>
+                <li>
+                  <img src={following} alt="" />
+                  <p>{user.following} Seguindo</p>
+                </li>
+                <li>
+                  <img src={repository} alt="" />
+                  <p>{user.public_repos} Repositórios</p>
+                </li>
+                <li>
+                  <img src={company} alt="" />
+                  <p>{user.company || "Estudante"}</p>
+                </li>
+                <li>
+                  <img src={location} alt="" />
+                  <p>{user.location}</p>
+                </li>
+              </ul>
+            </section>
+          </main>
           <footer>
             <div className="footer-logo">
               <img src={rocketLogo} alt="rocket logo" />
@@ -24,7 +79,7 @@ function App() {
       <div className="auxiliar-container">
         <div className="block">
           <p>Customizar Rocketcard</p>
-          <button>Gerar Background</button>
+          <button id="gerar-bg">Gerar Background</button>
         </div>
       </div>
     </div>
